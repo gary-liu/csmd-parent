cb6f2006179f94ea7bade737303d84820a810d72
gary <498982703@qq.com>
2020-03-11 15:47:03 +0800

    first commit

---
+++ src/main/java/com/csmd/parent/csmdparent/CsmdParentApplication.java
@@ -0,0 +1,13 @@
+package com.csmd.parent.csmdparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmdParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsmdParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csmd/parent/csmdparent/CsmdParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csmd.parent.csmdparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmdParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
